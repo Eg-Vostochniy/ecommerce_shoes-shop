927eb1b8f13fe9defd52e3b4b8c6c648cb2ef04b
siemens <zbukarv@gmail.com>
2022-07-25 19:16:01 +0300

    created login page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,17 +1,64 @@
+import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CustomInput } from '../components/CustomInput'
+import { useDispatch } from 'react-redux'
+import { login } from '../redux/actions/Auth'
+import { valid } from '../utils/authValidation'
 
 export const Login = () => {
-  window.scrollTo(0, 0)
+  const [isShowedPassword, setIsShowedPassword] = useState(false)
+  const [validErrs, setValidErrs] = useState([])
+
+  const form = useRef()
+
+  const dispatch = useDispatch()
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+
+    const { errs, errsLength } = valid.login(
+      form.current[0].value,
+      form.current[1].value
+    )
+
+    if (errsLength === 0) {
+      setValidErrs([])
+      dispatch(login(form.current[0].value, form.current[1].value))
+    } else setValidErrs(errs)
+  }
+
   return (
-    <div className=''>
-      <form>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
-        <button>Login</button>
-        <p>
-          <Link to='/register'>Create account</Link>
-        </p>
-      </form>
+    <div className='auth login'>
+      <div className='container'>
+        <form onSubmit={formSubmit} ref={form} className='auth__form'>
+          <h2>Sign In</h2>
+          <label className='auth__label email'>
+            <span>Email</span>
+            <CustomInput type='email' name='email' />
+          </label>
+          <label className='auth__label password'>
+            <span>Password</span>
+            <CustomInput
+              type={isShowedPassword ? 'text' : 'password'}
+              name='password'
+            />
+            <small onClick={() => setIsShowedPassword(!isShowedPassword)}>
+              {isShowedPassword ? 'Hide' : 'Show'}
+            </small>
+          </label>
+          <button type='submit'>Login</button>
+          <span className='auth__form-redirect'>
+            <Link to='/register'>Create account</Link>
+          </span>
+          {validErrs.length > 0 && (
+            <div>
+              {validErrs.map((error) => (
+                <div>* {error}</div>
+              ))}
+            </div>
+          )}
+        </form>
+      </div>
     </div>
   )
 }
