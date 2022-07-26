78ad1494e038be4d508565877df0eed2fd99d2d6
siemens <zbukarv@gmail.com>
2022-07-26 13:04:25 +0300

    chenged position styles auth pages

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -28,35 +28,37 @@ export const Login = () => {
   }
 
   return (
-    <form onSubmit={formSubmit} ref={form} className='auth__form login'>
-      <h2>Sign In</h2>
-      <label className='auth__label email'>
-        <span>Email</span>
-        <CustomInput type='email' name='email' />
-      </label>
-      <label className='auth__label password'>
-        <span>Password</span>
-        <CustomInput
-          type={isShowedPassword ? 'text' : 'password'}
-          name='password'
-        />
-        <small onClick={() => setIsShowedPassword(!isShowedPassword)}>
-          {isShowedPassword ? 'Hide' : 'Show'}
-        </small>
-      </label>
-      <button className='auth__form-button' type='submit'>
-        Login
-      </button>
-      <span className='auth__form-redirect'>
-        <Link to='/register'>Create account</Link>
-      </span>
-      {validErrs.length > 0 && (
-        <div className='auth__errors'>
-          {validErrs.map((error) => (
-            <div>* {error}</div>
-          ))}
-        </div>
-      )}
-    </form>
+    <div className='auth'>
+      <form onSubmit={formSubmit} ref={form} className='auth__form login'>
+        <h2>Sign In</h2>
+        <label className='auth__label email'>
+          <span>Email</span>
+          <CustomInput type='email' name='email' />
+        </label>
+        <label className='auth__label password'>
+          <span>Password</span>
+          <CustomInput
+            type={isShowedPassword ? 'text' : 'password'}
+            name='password'
+          />
+          <small onClick={() => setIsShowedPassword(!isShowedPassword)}>
+            {isShowedPassword ? 'Hide' : 'Show'}
+          </small>
+        </label>
+        <button className='auth__form-button' type='submit'>
+          Login
+        </button>
+        <span className='auth__form-redirect'>
+          <Link to='/register'>Create account</Link>
+        </span>
+        {validErrs.length > 0 && (
+          <div className='auth__errors'>
+            {validErrs.map((error) => (
+              <div>* {error}</div>
+            ))}
+          </div>
+        )}
+      </form>
+    </div>
   )
 }
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -35,39 +35,41 @@ export const Register = () => {
   }
 
   return (
-    <form onSubmit={formSubmit} ref={form} className='auth__form register'>
-      <h2>Sign Up</h2>
-      <label className='auth__label username'>
-        <span>User Name</span>
-        <CustomInput type='text' name='username' />
-      </label>
-      <label className='auth__label email'>
-        <span>Email</span>
-        <CustomInput type='email' name='email' />
-      </label>
-      <label className='auth__label password'>
-        <span>Password</span>
-        <CustomInput
-          type={isShowedPassword ? 'text' : 'password'}
-          name='password'
-        />
-        <small onClick={() => setIsShowedPassword(!isShowedPassword)}>
-          {isShowedPassword ? 'Hide' : 'Show'}
-        </small>
-      </label>
-      <button className='auth__form-button' type='submit'>
-        Register
-      </button>
-      <span className='auth__form-redirect'>
-        <Link to='/login'>Sign In</Link>
-      </span>
-      {validErrs.length > 0 && (
-        <div className='auth__errors'>
-          {validErrs.map((error) => (
-            <div>* {error}</div>
-          ))}
-        </div>
-      )}
-    </form>
+    <div className='auth'>
+      <form onSubmit={formSubmit} ref={form} className='auth__form register'>
+        <h2>Sign Up</h2>
+        <label className='auth__label username'>
+          <span>User Name</span>
+          <CustomInput type='text' name='username' />
+        </label>
+        <label className='auth__label email'>
+          <span>Email</span>
+          <CustomInput type='email' name='email' />
+        </label>
+        <label className='auth__label password'>
+          <span>Password</span>
+          <CustomInput
+            type={isShowedPassword ? 'text' : 'password'}
+            name='password'
+          />
+          <small onClick={() => setIsShowedPassword(!isShowedPassword)}>
+            {isShowedPassword ? 'Hide' : 'Show'}
+          </small>
+        </label>
+        <button className='auth__form-button' type='submit'>
+          Register
+        </button>
+        <span className='auth__form-redirect'>
+          <Link to='/login'>Sign In</Link>
+        </span>
+        {validErrs.length > 0 && (
+          <div className='auth__errors'>
+            {validErrs.map((error) => (
+              <div>* {error}</div>
+            ))}
+          </div>
+        )}
+      </form>
+    </div>
   )
 }
